dc3129b304506b7d8ca111edd10c342d65e35be9
Kirk Russell Montrose <krm2338@me.com>
2017-10-10 21:33:47 -0400

    Test Update

---
+++ Assets/BasicBullet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBullet : MonoBehaviour, IBullet {
+    Vector3 v = new Vector3();
+    // Use this for initialization
+    void Start () {
+        
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        double movementSpeed = BulletVelocity();
+
+       v.y = (float)  movementSpeed * Time.deltaTime;
+       transform.Translate(v);
+
+
+    }
+
+    public  double BulletVelocity()
+    {
+        return 50;
+    }
+
+    public bool HasProjectileArc()
+    {
+        return false;
+    }
+
+    public double BulletPower()
+    {
+        return 10;
+    }
+
+    public int BulletID()
+    {
+        return 1;
+    }
+
+    public int ColorOfExplsion()
+    {
+        return 1;
+    }
+}
---
+++ Assets/IBullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IBullet
+{
+    //Basic bullet params Kirk Montrose
+    double BulletVelocity();
+    bool HasProjectileArc();
+    double BulletPower();
+    int BulletID();
+    int ColorOfExplsion();
+}
